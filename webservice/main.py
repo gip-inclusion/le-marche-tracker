e83e682fb26dabff2b1785b4112f2e5235ff399d
pmontens <pieterjan@montens.net>
2021-07-23 15:59:49 +0200

    :wrench: removing misc action, more precise user_type action

--- webservice/main.py
+++ webservice/main.py
@@ -50,7 +50,7 @@ class Actions(str, Enum):
     directory_csv = 'directory_csv'
     adopt = 'adopt'
     adopt_search = 'adopt_search'
-    misc = 'misc'
+    user_type = 'user_type'
 
 
 class ClientContext(BaseModel):
